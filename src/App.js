8f99d74a9c6026f3b23e07c9525368d54afc69b0
Cozmu <www.jorgereis@gmail.com>
2022-11-29 16:14:41 -0300

    Add req 5 e 6 (sem teste)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Recipes from './pages/Recipes';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route path="/meals" component={ Recipes } />
+      <Route path="/drinks" />
+      <Route path="/profile" />
+      <Route path="/done-recipes" />
+      <Route path="/favorite-recipes" />
     </Switch>
   );
 }
