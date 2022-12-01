9699407a0393f3ecd1415fbabd989d32d2b1d290
Angela Samways <angelasamways@gmail.com>
2022-12-01 12:50:39 -0300

    Requisitos 57 a 62, faltando cobertura de testes

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
+// import Footer from '../components/Footer';
 
 function Profile() {
+  const history = useHistory();
+  const getEMail = () => {
+    const usuario = localStorage.getItem('user');
+    const email = JSON.parse(usuario);
+    if (email === null) {
+      return 'usuario@mail.com';
+    }
+    return email.email;
+  };
   return (
     <div>
       <Header />
+      <div>
+        <p data-testid="profile-email">
+          { getEMail() }
+        </p>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/done-recipes') }
+        >
+          Done Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/favorite-recipes') }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => {
+            localStorage.clear();
+            history.push('/');
+          } }
+        >
+          Logout
+        </button>
+      </div>
+      {/* <Footer /> */}
     </div>
   );
 }
