7358e8fd63eb32e80cbdf5403670a1e4514f7b32
Angela Samways <angelasamways@gmail.com>
2022-12-12 17:10:52 -0300

    tentando subir o percentual da cobertura

--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -14,8 +14,10 @@ const searchBtn = 'exec-search-btn';
 const nameSearchRadio = 'name-search-radio';
 const firstLetter = 'first-letter-search-radio';
 const ingredient = 'ingrediente-search-radio';
+const rf = 'recipe-photo';
+const rt = 'recipe-title';
 
-describe('Testa o componente SearchBar', () => {
+describe('Testa o componente SearchBar em Meal', () => {
   beforeEach(() => {
     global.fetch = jest.fn().mockResolvedValue({
       json: jest.fn().mockResolvedValue(oneMeal),
@@ -48,68 +50,22 @@ describe('Testa o componente SearchBar', () => {
     const nameRadio = screen.getByTestId(nameSearchRadio);
 
     act(() => {
-      userEvent.type(searchInp, 'Corba');
+      userEvent.type(searchInp, 'Arrabiata');
       userEvent.click(nameRadio);
       userEvent.click(searchButton);
     });
     await waitFor(() => {
-      expect(history.location.pathname).toEqual('/meals/52977');
+      expect(history.location.pathname).toEqual('/meals/52771');
     }, {
       timeout: 5000,
     });
-    const img = await screen.findByTestId('recipe-photo');
-    const title = await screen.findByTestId('recipe-title');
+    const img = await screen.findByTestId(rf);
+    const title = await screen.findByTestId(rt);
 
     expect(img).toBeInTheDocument();
     expect(title).toBeInTheDocument();
   });
-});
-describe('Testa o componente SearchBar-part2', () => {
-  beforeEach(() => {
-    global.fetch = jest.fn().mockResolvedValue({
-      json: jest.fn().mockResolvedValue(drinks),
-    });
-  });
-  afterEach(() => {
-    global.fetch.mockClear();
-  });
-
-  test('2-Testa a pesquisa pelo nome de uma bebida', async () => {
-    const { history } = renderWithRouter(
-      <RecipesAppProvider>
-        <App />
-      </RecipesAppProvider>,
-    );
-    act(() => {
-      history.push('/drinks');
-    });
-    const searchIcon = screen.getByTestId(searchTopBtn);
-    act(() => {
-      userEvent.click(searchIcon);
-    });
-    const searchInp = screen.getByTestId(searchInput);
-    const searchButton = screen.getByTestId(searchBtn);
-    const ingredients = screen.getByTestId(ingredient);
-
-    act(() => {
-      userEvent.type(searchInp, 'Amaretto Tea');
-      userEvent.click(ingredients);
-      userEvent.click(searchButton);
-    });
-
-    await waitFor(() => {
-      expect(history.location.pathname).toEqual('/drinks/11029');
-    }, {
-      timeout: 5000,
-    });
-    const img = await screen.findByTestId('recipe-photo');
-    const title = await screen.findByTestId('recipe-title');
-
-    expect(img).toBeInTheDocument();
-    expect(title).toBeInTheDocument();
-  });
-
-  test('3-Testa a pesquisa por 1a letra', async () => {
+  test('2-Testa a pesquisa por 1a letra', async () => {
     const { history } = renderWithRouter(
       <RecipesAppProvider>
         <App />
@@ -140,7 +96,118 @@ describe('Testa o componente SearchBar-part2', () => {
     });
   });
 });
-describe('Testa o componente SearchBar-part3', () => {
+describe('Testa o componente SearchBar em Drinks', () => {
+  beforeEach(() => {
+    global.fetch = jest.fn().mockResolvedValue({
+      json: jest.fn().mockResolvedValue(drinks),
+    });
+  });
+  afterEach(() => {
+    global.fetch.mockClear();
+  });
+
+  test('3-Testa a pesquisa pelo nome de uma bebida', async () => {
+    const { history } = renderWithRouter(
+      <RecipesAppProvider>
+        <App />
+      </RecipesAppProvider>,
+    );
+    act(() => {
+      history.push('/drinks');
+    });
+    const searchIcon = screen.getByTestId(searchTopBtn);
+    act(() => {
+      userEvent.click(searchIcon);
+    });
+    const searchInp = screen.getByTestId(searchInput);
+    const searchButton = screen.getByTestId(searchBtn);
+    const ingredients = screen.getByTestId(ingredient);
+
+    act(() => {
+      userEvent.type(searchInp, 'GG');
+      userEvent.click(ingredients);
+      userEvent.click(searchButton);
+    });
+
+    await waitFor(() => {
+      expect(history.location.pathname).toEqual('/drinks/15997');
+    }, {
+      timeout: 5000,
+    });
+    const img = await screen.findByTestId(rf);
+    const title = await screen.findByTestId(rt);
+
+    expect(img).toBeInTheDocument();
+    expect(title).toBeInTheDocument();
+  });
+  test('4-Muda para a rota de detalhes quando a API só retorna um resultado', async () => {
+    const { history } = renderWithRouter(
+      <RecipesAppProvider>
+        <App />
+      </RecipesAppProvider>,
+    );
+
+    act(() => {
+      history.push('/drinks');
+    });
+
+    const searchIcon = screen.getByTestId(searchTopBtn);
+
+    act(() => {
+      userEvent.click(searchIcon);
+    });
+
+    const searchInp = screen.getByTestId(searchInput);
+    const searchButton = screen.getByTestId(searchBtn);
+    const nameRadio = screen.getByTestId(nameSearchRadio);
+
+    act(() => {
+      userEvent.type(searchInp, 'Skirt');
+      userEvent.click(nameRadio);
+      userEvent.click(searchButton);
+    });
+
+    await waitFor(() => {
+      expect(history.location.pathname).toBe('/drinks/11433');
+    }, { timeout: 5000 });
+  });
+  test('5-Testa a pesquisa pelo nome de um ingrediente', async () => {
+    const { history } = renderWithRouter(
+      <RecipesAppProvider>
+        <App />
+      </RecipesAppProvider>,
+    );
+    act(() => {
+      history.push('/drinks');
+    });
+    const searchIcon = screen.getByTestId(searchTopBtn);
+    act(() => {
+      userEvent.click(searchIcon);
+    });
+    const searchInp = screen.getByTestId(searchInput);
+    const searchButton = screen.getByTestId(searchBtn);
+    const ingredients = screen.getByTestId(ingredient);
+
+    act(() => {
+      userEvent.type(searchInp, 'Galliano');
+      userEvent.click(ingredients);
+      userEvent.click(searchButton);
+    });
+
+    await waitFor(() => {
+      expect(history.location.pathname).toEqual('/drinks/15997');
+    }, {
+      timeout: 5000,
+    });
+    const img = await screen.findByTestId(rf);
+    const title = await screen.findByTestId(rt);
+
+    expect(img).toBeInTheDocument();
+    expect(title).toBeInTheDocument();
+  });
+});
+
+describe('Testa o alerta no componente SearchBar', () => {
   beforeEach(() => {
     global.fetch = jest.fn().mockResolvedValue({
       json: jest.fn().mockResolvedValue({
@@ -152,7 +219,7 @@ describe('Testa o componente SearchBar-part3', () => {
     global.fetch.mockClear();
   });
 
-  test('4-Testa se aparece o alerta ao digitar mais de 1 letra, ao selecionar o radio first letter', async () => {
+  test('6-Testa se aparece o alerta ao digitar mais de 1 letra, ao selecionar o radio first letter', async () => {
     jest.spyOn(window, 'alert').mockImplementation(() => {});
 
     const { history } = renderWithRouter(
