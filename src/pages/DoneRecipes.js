312ff04350eca58d6f1aadfae81466618aabd20b
Cozmu <www.jorgereis@gmail.com>
2022-12-13 16:01:37 -0300

    Add req 44

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -32,7 +32,50 @@ function DoneRecipes() {
           Drinks
         </button>
       </section>
-
+      {finishedRecipes?.map((element, index) => (
+        <section
+          key={ index }
+        >
+          <img
+            data-testid={ `${index}-horizontal-image` }
+            width="250px"
+            src={ element.image }
+            alt={ element.id }
+            id={ element.tags[0] }
+          />
+          <h4
+            data-testid={ `${index}-horizontal-top-text` }
+          >
+            {element.category}
+          </h4>
+          <h1
+            data-testid={ `${index}-horizontal-name` }
+          >
+            {element.name}
+          </h1>
+          <h4
+            data-testid={ `${index}-horizontal-done-date` }
+          >
+            {element.doneDate}
+          </h4>
+          <button
+            data-testid={ `${index}-horizontal-share-btn` }
+            type="button"
+          >
+            Compartilhar
+          </button>
+          {element.tags.length > 1
+          && element?.tags.map((tagName, i) => (
+            <div key={ i }>
+              <p
+                data-testid={ `${index}-${tagName}-horizontal-tag` }
+              >
+                {tagName}
+              </p>
+            </div>
+          ))}
+        </section>
+      ))}
     </div>
   );
 }
